11f18e813782c9f2558de26517855da288e33f17
Krishna00008 <156284133+Krishna00008@users.noreply.github.com>
2024-02-27 15:10:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ import io
 
 API_ID = 10577960
 API_HASH = "80fd047285f4e94ca80311928b6bb5da"
-BOT_TOKEN = "6150009394:AAH-AewKKW_9n8a9qSDOl9rxbMo7dErrKCE"
+BOT_TOKEN = "6590553618:AAHkfWDyjAO7nHStVc85wTirkun0Ab-4PY4"
 bot = Client(
     "bot",
     bot_token=BOT_TOKEN,
